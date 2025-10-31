df27340c5050fbb2cda3737cdf29a33265da9251
Robert Sianturi <40648746+robertsianturi@users.noreply.github.com>
2025-11-01 07:56:33 +1030

    Create problem181.sql

---
+++ sql/problem181.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+# 181. Employees Earning More Than Their Managers
+# https://leetcode.com/problems/employees-earning-more-than-their-managers/
+SELECT 
+    e1.name AS Employee
+FROM 
+    Employee AS e1
+INNER JOIN
+    Employee AS e2
+    ON e1.managerId = e2.id
+WHERE 
+    e1.salary > e2.salary
